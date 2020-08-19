01d2cd5252246c09b6a504dd36ef128431598e57
farhadali <alifarhad557@gmail.com>
2020-08-19 15:24:31 +0500

    init

---
+++ public/index.js
@@ -0,0 +1,23 @@
+// const burger = document.querySelector("#burger");
+// const menu = document.querySelector("#menu");
+
+
+// burger.addEventListener('click', () => {
+//     if(menu.classList.contains('hidden')) {
+//         menu.classList.remove('hidden');
+//     } else {
+//         menu.classList.add('hidden');
+//     }
+// })
+
+
+  var typed = new Typed('.write', {
+    strings: ["Developer", "Coder", "Designer", "Freelancer", "Writer", "Secret-Ninja ", "Bibliophile", "Potter Head", "Tea Lover", "Marvel Fan"],
+    typeSpeed: 50,
+    backSpeed: 20,
+    backDelay: 500,
+    startDelay: 1000,
+    loop: true,
+    showCursor: false,
+
+  });
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: {
+    // enabled: true,
+    // content: ['public/index.html'],
+
+  },
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0BE9B4",
+        secondary: {
+          100: "#2BBE9B",
+          200: "#0BE9B4",
+          300: "#2BBE9B",
+        }
+      },
+      fontFamily: {
+        body: ['Poppins']
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
