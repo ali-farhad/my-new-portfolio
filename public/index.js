466bbac63a7158d64f5db470a7f38fb983b35591
farhadali <alifarhad557@gmail.com>
2020-08-21 12:38:17 +0500

    added portfolio page

--- public/index.js
+++ public/index.js
@@ -1,18 +1,18 @@
-// const burger = document.querySelector("#burger");
-// const menu = document.querySelector("#menu");
+const burger = document.querySelector("#burger");
+const menu = document.querySelector("#menu");
 
 
-// burger.addEventListener('click', () => {
-//     if(menu.classList.contains('hidden')) {
-//         menu.classList.remove('hidden');
-//     } else {
-//         menu.classList.add('hidden');
-//     }
-// })
+burger.addEventListener('click', () => {
+    if(menu.classList.contains('hidden')) {
+        menu.classList.remove('hidden');
+    } else {
+        menu.classList.add('hidden');
+    }
+})
 
 
   var typed = new Typed('.write', {
-    strings: ["Developer", "Coder", "Designer", "Freelancer", "Writer", "Secret-Ninja ", "Bibliophile", "Potter Head", "Tea Lover", "Marvel Fan"],
+    strings: ["Developer", "Coder", "Designer", "Freelancer", "Writer", "Bibliophile", "Potter Head", "Tea Lover", "Marvel Fan"],
     typeSpeed: 50,
     backSpeed: 20,
     backDelay: 500,
